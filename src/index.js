406b273ac032a5f4a57a72b91adc9c53fc98cbfd
Diego Herrada <diegoherrada@outlook.com>
2020-11-19 15:12:32 -0500

    Adding auth

--- src/index.js
+++ src/index.js
@@ -1,55 +1,17 @@
+const { PrismaClient } = require('@prisma/client')
 const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Link = require('./resolvers/Link')
+const User = require('./resolvers/User')
 
-// typeDefinitions
-/* The typeDefs constant defines your GraphQL schema. Here, it defines a simple Query type with one field called info.
-This field has the type String!. The exclamation mark in the type definition means that this field is required and can
-never be null.
- */
-
-// hardcoded links for now
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-// Resolvers
-/*
-The resolvers object is the actual implementation of the GraphQL schema.
-Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
- */
-
-// temporary unique Id generator
-let idCount = links.length
+const prisma = new PrismaClient()
 
 const resolvers = {
-    Query: {
-        info: ()=> 'This is the HackerNewsCone API',
-        feed: ()=> links
-    },
-    Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            console.log("Successfully created")
-            return link
-        },
-        update: (parent, args) => {
-            const link = {
-                id: args.id,
-                description: args.description,
-                url: args.url
-            }
-            return links.splice(0, args.id, link)
-        },
-        delete: (parent, args) => {
-            return links.splice(0, args.id)
-        }
-    }
+    Query,
+    Mutation,
+    Link,
+    User
 }
 
 // Server
@@ -59,7 +21,13 @@ This tells the server what API operations are accepted and how they should be re
  */
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma
+        }
+    }
 })
 server.start(()=> console.log('Server is running on http://localhost:4000'))
 
