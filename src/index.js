b27615c9e4dc50992259d669750343abbf4bf01f
Diego Herrada <diegoherrada@outlook.com>
2021-01-02 04:00:11 -0500

    minor fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const { PrismaClient } = require('@prisma/client')
-const { ApolloServer } = require('apollo-server')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Link = require('./resolvers/Link')
-const User = require('./resolvers/User')
-const fs = require('fs')
-const path = require('path')
-require('dotenv').config()
+const { PrismaClient } = require('@prisma/client');
+const { ApolloServer } = require('apollo-server');
+const { PubSub } = require('apollo-server')
+const schema = require('./schema/schema');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Link = require('./resolvers/Link');
+const User = require('./resolvers/User');
+const {getUserId} = require('./utils')
+require('dotenv').config();
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
 
 const resolvers = {
     Query,
@@ -24,12 +25,14 @@ Finally, the schema and resolvers are bundled and passed to the GraphQLServer wh
 This tells the server what API operations are accepted and how they should be resolved.
  */
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+    typeDefs:  schema,
     resolvers,
     context: request => {
         return {
             ...request,
-            prisma
+            prisma,
+            pubsub,
+            userId: request && request.headers.authorization ? getUserId(request) : null
         }
     }
 })
